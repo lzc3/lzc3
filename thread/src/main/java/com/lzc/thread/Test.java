a98a714667e3cc87b7dac56842deb7c0dd115b61
李昭灿 <zhaocan.li@definesys.com>
2024-11-19 22:15:38 +0800

    "初始化设计模式项目"

--- thread/src/main/java/com/lzc/thread/Test.java
+++ thread/src/main/java/com/lzc/thread/Test.java
@@ -1,4 +1,27 @@
 package com.lzc.thread;
 
 public class Test {
+
+    private static volatile boolean initFlag = false;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        new Thread(() -> {
+            System.out.println("waiting data");
+            while (!initFlag) {
+
+            }
+            System.out.println("end thread");
+        }).start();
+
+        Thread.sleep(1000);
+
+        new Thread(Test::prepareData).start();
+    }
+
+
+    public static void prepareData() {
+        initFlag = true;
+    }
+
 }
