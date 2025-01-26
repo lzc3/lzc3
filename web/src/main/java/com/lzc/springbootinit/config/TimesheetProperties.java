bc708fc8fcc59937c1c27f031197f5d3fa81b133
李昭灿 <zhaocan.li@definesys.com>
2025-01-26 22:49:11 +0800

    "feat(spring模块)"

---
+++ web/src/main/java/com/lzc/springbootinit/config/TimesheetProperties.java
@@ -0,0 +1,30 @@
+package com.lzc.springbootinit.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@Component
+@PropertySource("classpath:timesheet.properties")
+@ConfigurationProperties(prefix = "timesheet")
+public class TimesheetProperties {
+
+    String url = "";
+
+    String tenantid = "241251302414221313";
+    String xdapappid = "291278919397539840";
+    String timeZone = "+08:00";
+    String token;
+
+    String monthDayUuid;
+    String weekDayUuid;
+
+    Integer year;
+    Integer month;
+
+    List<String> blackMonthDay;
+}
