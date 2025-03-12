7c6e926f5d2f19a9e233458426a593ad12440b0b
李昭灿 <zhaocan.li@definesys.com>
2025-03-12 21:19:38 +0800

    "feat(添加本地ocr模块)"

---
+++ ocr/src/main/java/com/ocr/BasicExample.java
@@ -0,0 +1,41 @@
+package com.ocr;
+
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.leptonica.PIX;
+import org.bytedeco.tesseract.TessBaseAPI;
+
+import java.io.IOException;
+
+import static org.bytedeco.leptonica.global.leptonica.pixDestroy;
+import static org.bytedeco.leptonica.global.leptonica.pixRead;
+
+public class BasicExample {
+    public static void main(String[] args) throws IOException {
+        BytePointer outText;
+
+        TessBaseAPI api = new TessBaseAPI();
+        // Initialize tesseract-ocr with English, without specifying tessdata path
+        if (api.Init("C:\\lzc\\app\\proTools\\ai\\temp\\tessdata", "chi_sim") != 0) {
+            System.err.println("Could not initialize tesseract.");
+            System.exit(1);
+        }
+
+        // Open input image with leptonica library
+        //  "image.png"
+
+//        MultipartFile multipartFile = null;
+//        byte[] imageBytes = multipartFile.getBytes();
+//        PIX image2 = pixReadMem(imageBytes, imageBytes.length);
+
+        PIX image = pixRead(args.length > 0 ? args[0] : "C:\\lzc\\img\\ocr\\image.png");
+        api.SetImage(image);
+        // Get OCR result
+        outText = api.GetUTF8Text();
+        System.out.println("OCR output:\n" + outText.getString());
+
+        // Destroy used object and release memory
+        api.End();
+        outText.deallocate();
+        pixDestroy(image);
+    }
+}
---
+++ ocr/src/main/java/com/ocr/Tess4j.java
@@ -0,0 +1,24 @@
+package com.ocr;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Tess4j {
+    public static void main(String[] args) throws TesseractException, IOException {
+        // todo test file
+        // 将图片保存为临时文件
+        File tempFile = File.createTempFile("temp-", "test");
+//        file.transferTo(tempFile);
+
+        Tesseract tesseract = new Tesseract();
+        tesseract.setDatapath("src/main/resources/tessdata"); // 训练数据路径
+        tesseract.setLanguage("eng+chi_sim"); // 中英文识别
+
+        String result = tesseract.doOCR(tempFile);
+        tempFile.delete(); // 清理临时文件
+        System.out.println(result);
+    }
+}
